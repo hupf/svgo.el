07f20b9fc0e2d7b90ae11d174990e79b703ce339
Mathis Hofer <mathis@fsfe.org>
2022-04-27 22:51:16 +0200

    Fix typo and add license header to test

--- test/test-svgo.el
+++ test/test-svgo.el
@@ -1,11 +1,26 @@
 ;;; test-svgo.el --- Tests for svgo.el
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
 ;;; Commentary:
 
 ;;; The following tests rely a bit heavily on mocks, but since we want
 ;;; to avoid the side-effects of the actual installing of the NPM
 ;;; package, this serves the purpose of at least testing the
-;;; functions' internal logic.
+;;; function's internal logic.
 
 ;;; Code:
 
