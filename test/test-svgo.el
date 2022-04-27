240b6e13c451438c60e293bacee9b5e629f4b329
Mathis Hofer <mathis@fsfe.org>
2022-04-27 22:52:46 +0200

    Use lexical binding in test

--- test/test-svgo.el
+++ test/test-svgo.el
@@ -1,4 +1,4 @@
-;;; test-svgo.el --- Tests for svgo.el
+;;; test-svgo.el --- Tests for svgo.el -*- lexical-binding: t; -*-
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
